3751bab541ec156a0981c52db4dbcbd7fcf5c54f
Jay Landers <jay@jaybirdgroup.net>
2021-02-20 17:20:08 -0700

    added transaction and balance checking functionality

--- transaction.py
+++ transaction.py
@@ -6,19 +6,58 @@ import decrypt_key
 from generate_wallet import get_address, get_public_key
 import dai_abi
 
-w3 = Web3(Web3.HTTPProvider(endpoint))
-w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+def make_txn(destination, pk, amount):
+    txn = dict(
+    nonce = w3.eth.getTransactionCount(address(pk)),
+    gasPrice = w3.eth.gasPrice,
+    gas = 100000,
+    to = destination,
+    value = amount,
+    data = b'',
+    chainId = 4
+    )
 
-print(w3.isConnected())
+    signed_txn = w3.eth.account.signTransaction(txn, private_key)
 
-private_key, *_  = decrypt_key.get_key()
+    return signed_txn
 
-abi = json.loads(dai_abi.abi)
-dai_address = '0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8'
-dai = w3.eth.contract(address = dai_address, abi=abi)
+def login():
+    private_key, *_  = decrypt_key.get_key()
+    return private_key
 
-print(dai.functions.totalSupply().call())
+if __name__ == '__main__':
+    w3 = Web3(Web3.HTTPProvider(endpoint))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-address = lambda x: get_address(get_public_key(x))
+    if w3.isConnected():print('Connected')
 
-print(w3.eth.get_balance(address(private_key)))
+    private_key = login()
+
+    abi = json.loads(dai_abi.abi)
+    dai_address = '0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8'
+    dai = w3.eth.contract(address = dai_address, abi=abi)
+
+    print(dai.functions.totalSupply().call())
+
+    address = lambda x: get_address(get_public_key(x))
+
+    #print(w3.eth.get_balance(address(private_key)))
+    #print(w3.eth.getTransactionCount(address(private_key)))
+
+
+    #txn = make_txn('0x6D677F67d6C0ceE0Be87dBEB701C1577361799b8', private_key, 1)
+    #txn_hash = w3.eth.sendRawTransaction(txn.rawTransaction)
+    #print(txn_hash)
+
+    while True:
+        action = input('Type 1 to send transaction and 2 to check balance or 3 to logout\n')
+        if action == '1':
+            receiver = input('To whom?\n')
+            amt = int(float(input('For what amount?(ETH)\n'))*1_000_000_000_000_000_000)
+            txn = make_txn(receiver, private_key, amt)
+            txn_hash = w3.eth.sendRawTransaction(txn.rawTransaction)
+            print(f"Hash: {txn_hash.hex()}")
+        elif action == '2':
+            print(w3.eth.get_balance(address(private_key))/1000000000000000000)
+        elif action == '3':
+            exit()
